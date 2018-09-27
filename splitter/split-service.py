5a0f932dac5adce9b415a292a2970637287f485a
Roman Klimenko <romank@marmetrix.com>
2018-09-26 21:50:56 -0700

    splitter/detector/uploader

--- splitter/split-service.py
+++ splitter/split-service.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numpy as np
+from numpy import array
 import argparse
 import cv2
 import time
@@ -26,10 +28,11 @@ def fileIsReady(file):
 def initArguments():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--in", required=True, help="folder with new images")
-    # ap.add_argument("-o", "--out", required=True, help="folder with processed images")
-    # ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-    # ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
-    # ap.add_argument("-c", "--confidence", type=float, default=0.7, help="minimum probability to filter weak detections")
+    ap.add_argument("-e", "--extension", required=True, help="extension of video file")
+    ap.add_argument("-b", "--bucket", required=True, help="bucket for frames")
+    ap.add_argument("-p", "--prefix", required=True, help="prefix for frames")
+    ap.add_argument("-t", "--prototxt", required=True, help="prototxt file")
+    ap.add_argument("-m", "--model", required=True, help="model file")
     return vars(ap.parse_args())
 
 
@@ -49,17 +52,23 @@ def get_frames(fileName,extension):
 
         # capture 1 frame per second
         count = 0
+        index = 0
         success, frame = video.read()
         while success:
-            success, frame = video.read()
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
+            compressedFrame = cv2.imencode('.jpg', frame, encode_param)
 
             if count % fps == 0:
-                suffix = "-%#05d.jpg" % (count + 1)
-                outputFile = fileName.replace(extension, suffix)
-                cv2.imwrite(outputFile, frame)
-                np.append(frames, outputFile)
+                index = index + 1
+                suffix = "-%#05d.jpg" % index
+                outputFile = fileName.replace(".%s" % extension, suffix)
+                cv2.imwrite(outputFile, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+                frames.append(outputFile)
+
                 print(outputFile)
+
             count = count + 1
+            success, frame = video.read()
 
         video.release()
 
@@ -69,40 +78,84 @@ def get_frames(fileName,extension):
 
     return frames
 
-def upload_frames(files,bucket,prefix):
+def upload_frames(files,source,bucket,prefix):
     s3 = boto3.client('s3')
     for file in files:
-        key = "%s/%s" % (prefix, file)
+        key = "%s/%s" % (prefix, file.replace(source,''))
         print("uploading to %s %s" % (bucket, key))
-        s3.upload_file(file, bucket, key)                                
-    
-def process_videos(source, extension):
-    processed = 0
-    videos = findFiles(source, extension)
-    
-    if len(videos) > 0:
-        print("found ", videos)
+        s3.upload_file(file, bucket, key)
 
-        for file in videos:
-            fileName = join(source, file)
-            frames = get_frames (file,extension)
-            print(frames)
-            upload_frames(frames,"ecorvid","frames")
+def get_frames_with_objects(frames,confidenceThreshold):
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+               "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+               "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+               "sofa", "train", "tvmonitor"]
+
+    images = []
+    for file in frames:
+        try:
+            image = cv2.imread(file)
+            (h, w) = image.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+            net.setInput(blob)
+            detections = net.forward()
+            objects_detected = 0
+            label = ""
+
+            # loop over the detections
+            for i in np.arange(0, detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
+
+                if confidence > confidenceThreshold:
+                    idx = int(detections[0, 0, i, 1])
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+
+                    label = label + "{}: {:.2f}% ; ".format(CLASSES[idx], confidence * 100)
+                    objects_detected = objects_detected + 1
+
+            if objects_detected>0 :
+                images.append([file,label])
+
+        except:
+            print("[processFrames]: Unexpected error:", sys.exc_info()[0])
+            raise
+
+    return images
 
 
 ###################################################
 
 args = initArguments()
 
-# load our serialized model from disk
-timestamp1 = dt.datetime.now()
+sourceFolder = args["in"]
+sourceExtension = args["extension"]
+framesBucket = args["bucket"]
+framesPrefix = args["prefix"]
+protoTxt = args["prototxt"]
+model = args["model"]
+
+
+
+print("loading caffe model...")
+net = cv2.dnn.readNetFromCaffe(protoTxt, model)
 
 print("waiting for incoming files...")
-
 try:
     # while True:
+        videos = findFiles(sourceFolder, sourceExtension)
 
-        process_videos(args["in"], ".264")
+        if len(videos) > 0:
+            print("found ", videos)
+            for file in videos:
+                images = get_frames("%s%s" % (sourceFolder, file), sourceExtension)
+                images_with_objects = get_frames_with_objects(images,0.3)
+                if len(images_with_objects) > 0:
+                    findings = array(images_with_objects)[:,1:].flatten()
+                    images_to_upload = array(images_with_objects)[:,:1].flatten()
+                    upload_frames(images_to_upload, sourceFolder, framesBucket, framesPrefix)
+
         time.sleep(1)
 
 except KeyboardInterrupt:
