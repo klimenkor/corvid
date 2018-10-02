f3704cba14d639665342a36e852017c8085d2f63
Roman Klimenko <roman.klimenko@castingnetworks.com>
2018-10-01 19:51:01 -0700

    attachment

--- mail-catcher/handler.py
+++ mail-catcher/handler.py
@@ -5,29 +5,39 @@ import time
 import email
  
 def catch_email(event, context):
-    mailBucket = 'corvid-mailbox'
-    frameBucket = 'ecorvid-frames'
+    try:
+        mailBucket = 'corvid-mailbox'
+        frameBucket = 'ecorvid-frames'
 
-    s3 = boto3.resource('s3')
-    ses = event["Records"][0]["ses"]
-    messageId = ses["mail"]['messageId']
-    print('>>>>got message %s' % (messageId))
+        s3 = boto3.resource('s3')
+        ses = event["Records"][0]["ses"]
+        messageId = ses["mail"]['messageId']
+        print('>>>>got message %s' % (messageId))
 
-    message = email.message_from_string(s3.Object(mailBucket, messageId).get()['Body'].read().decode('utf-8'))
-    attachment = message.get_payload()[1]
-    data = attachment.get_payload(decode=True)
+        message = email.message_from_string(s3.Object(mailBucket, messageId).get()['Body'].read().decode('utf-8'))
+        attachment = message.get_payload()[1]
+        data = attachment.get_payload(decode=True)
 
-    s3client = boto3.client('s3')
-    s3client.put_object(Bucket=frameBucket,Key= messageId,ContentType='image/jpeg',Body=data)
-    print('>>>>>saved attachment')
+        s3client = boto3.client('s3')
+        s3client.put_object(Bucket=frameBucket,Key= messageId,ContentType='image/jpeg',Body=data)
+        print('>>>>>saved attachment')
 
-    body = {
-        "message": "Attachment (%dbytes) saved as %s in bucket [%s]" % (len(data),messageId,frameBucket)
-    }
+        body = {
+            "message": "Attachment (%dbytes) saved as %s in bucket [%s]" % (len(data),messageId,frameBucket)
+        }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+    except Exception as e:
+        body = {
+            "message": "Exception!!!"
+        }
+
+        response = {
+            "statusCode": 404,
+            "body": json.dumps(e)
+        } 
 
     return response
