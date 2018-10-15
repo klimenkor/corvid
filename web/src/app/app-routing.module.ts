14ff676a1c35a25f4204acb6d54218190fa8a527
Roman Klimenko <roman.klimenko@castingnetworks.com>
2018-10-15 14:51:30 -0700

    added auth draft

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,13 +3,34 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
-import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
+import { HomeComponent, AboutComponent, HomeLandingComponent } from './public/home.component';
+import { RegistrationConfirmationComponent } from './public/auth/confirm/confirmRegistration.component';
+import { ResendCodeComponent } from './public/auth/resend/resendCode.component';
+import { ForgotPassword2Component, ForgotPasswordStep1Component } from './public/auth/forgot/forgotPassword.component';
+import { NewPasswordComponent } from './public/auth/newpassword/newpassword.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {path: 'about', component: AboutComponent},
+            {path: 'login', component: LoginComponent},
+            {path: 'register', component: RegisterComponent},
+            {path: 'confirmRegistration/:username', component: RegistrationConfirmationComponent},
+            {path: 'resendCode', component: ResendCodeComponent},
+            {path: 'forgotPassword/:email', component: ForgotPassword2Component},
+            {path: 'forgotPassword', component: ForgotPasswordStep1Component},
+            {path: 'newPassword', component: NewPasswordComponent},
+            {path: '', component: HomeLandingComponent},
+            {path: 'dashboard', component: DashboardComponent }
+        ]
+    }
 ];
 
 @NgModule({
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -12,14 +12,37 @@ import { ForgotPasswordComponent } from './views/forgot-password/forgot-password
 import { LoggerService } from './service/common/logger.service';
 import { ConsoleLoggerService } from './service/common/console-logger.service';
 import { FormsModule } from '@angular/forms';
+import { LogoutComponent, RegistrationConfirmationComponent } from './public/auth/confirm/confirmRegistration.component';
+import { NewPasswordComponent } from './public/auth/newpassword/newpassword.component';
+import { ResendCodeComponent } from './public/auth/resend/resendCode.component';
+import { ForgotPasswordStep1Component, ForgotPassword2Component } from './public/auth/forgot/forgotPassword.component';
+import { MFAComponent } from './public/auth/mfa/mfa.component';
+import { AboutComponent, HomeLandingComponent, HomeComponent } from './public/home.component';
+import { UseractivityComponent } from './secure/useractivity/useractivity.component';
+import { MyProfileComponent } from './secure/profile/myprofile.component';
+import { SecureHomeComponent } from './secure/landing/securehome.component';
+import { JwtComponent } from './secure/jwttokens/jwt.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    NewPasswordComponent,
     LoginComponent,
-    DashboardComponent,
+    LogoutComponent,
+    RegistrationConfirmationComponent,
+    ResendCodeComponent,
+    ForgotPasswordStep1Component,
+    ForgotPassword2Component,
     RegisterComponent,
-    ForgotPasswordComponent
+    MFAComponent,
+    AboutComponent,
+    HomeLandingComponent,
+    HomeComponent,
+    UseractivityComponent,
+    MyProfileComponent,
+    SecureHomeComponent,
+    JwtComponent,
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
