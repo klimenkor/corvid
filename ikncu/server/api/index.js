80d2eecf4e7af311cf8f0287e48f2af18fee8555
Roman Klimenko <roman.klimenko@castingnetworks.com>
2019-03-26 14:27:01 -0700

    image api draft

--- ikncu/server/api/index.js
+++ ikncu/server/api/index.js
@@ -13,27 +13,45 @@ var documentClient = new AWS.DynamoDB.DocumentClient();
  */
 exports.handler = (event, context, callback) => {
     const operation = event.operation;
-    const table = tableNames.getName(event.table);
-    event.tableName = table;
-    event.payload.TableName = event.tableName;
-    console.log(event);
-    switch (operation) {
-        case 'create':
-            documentClient.put(event.payload, callback);
-            break;
-        case 'read':
-            documentClient.get(event.payload, callback);
-            break;
-        case 'update':
-            documentClient.update(event.payload, callback);
-            break;
-        case 'delete':
-            documentClient.delete(event.payload, callback);
-            break;
-        case 'list':
-            documentClient.query(event.payload, callback);
-            break;
-        default:
-            callback(new Error(`Unrecognized operation "${operation}"`));
+    if(operation === "image")
+    {
+        const sourceBucket = event.sourceBucket;
+        const destinationBucket = event.destinationBucket;
+        const sourceFile = event.sourceFile; 
+        const destinationFile = event.destinationFile;
+        const width = event.width;
+        const height = event.height;    
+        console.log(sourceBucket,destinationBucket,sourceFile,destinationFile,width,height);
+        const res = sourceBucket + "/" + destinationBucket + "/" + sourceFile + "/" + destinationFile + "/" + width + "/" + height
+        callback({
+            'statusCode': 200,
+            'body': json.dumps(res)
+        });
+    }
+    else // database generic operations
+    {
+        const table = tableNames.getName(event.table);
+        event.tableName = table;
+        event.payload.TableName = event.tableName;
+        console.log(event);
+        switch (operation) {
+            case 'create':
+                documentClient.put(event.payload, callback);
+                break;
+            case 'read':
+                documentClient.get(event.payload, callback);
+                break;
+            case 'update':
+                documentClient.update(event.payload, callback);
+                break;
+            case 'delete':
+                documentClient.delete(event.payload, callback);
+                break;
+            case 'list':
+                documentClient.query(event.payload, callback);
+                break;
+            default:
+                callback(new Error(`Unrecognized operation "${operation}"`));
+        }
     }
 };
